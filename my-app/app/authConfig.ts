71f9c438ceab08124762724639bbbe74f51d6557
Sameer Shaboodien <sameer@Sameers-MacBook-Pro.local>
2025-04-05 23:10:56 +0200

    minor correction

--- my-app/app/authConfig.ts
+++ my-app/app/authConfig.ts
@@ -1,8 +1,10 @@
 export const cognitoAuthConfig = {
-    authority: "https://us-east-1dlb9dc7ko.auth.us-east-1.amazoncognito.com",
+    authority: "https://dlb9dc7ko.auth.us-east-1.amazoncognito.com",
     client_id: "79ufsa70isosab15kpcmlm628d",
     redirect_uri: "https://telematicshub.vercel.app",
     response_type: "code",
     scope: "email openid phone",
   };
   
+
+    
