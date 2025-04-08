eec08454d4094877b8958be0cd25323924d52ca5
Sameer Shaboodien <sameer@Sameers-MacBook-Pro.local>
2025-04-08 20:47:58 +0200

    main url

--- my-app/app/authConfig.ts
+++ my-app/app/authConfig.ts
@@ -1,7 +1,7 @@
 export const cognitoAuthConfig = {
     authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_dlB9DC7Ko",
     client_id: "79ufsa70isosab15kpcmlm628d",
-    redirect_uri: "http://localhost:3000",//https://telematicshub.vercel.app",
+    redirect_uri: "http://telematicshub.vercel.app",//https://telematicshub.vercel.app",
     response_type: "code",
     scope: "email openid phone",
   };
